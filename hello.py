7e46e72da6e1f973f3909b902dbdf7d4ce50e71e
rohyeongeon <66245679+rohyeongeon@users.noreply.github.com>
2022-09-20 16:12:03 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
+217@217-34 MINGW64 /c/git/03w (master)
+$ git init
+Reinitialized existing Git repository in C:/git/03w/.git/
+
+217@217-34 MINGW64 /c/git/03w (master)
+$ git remote add oigin https://github.com
 
